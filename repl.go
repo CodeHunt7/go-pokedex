e1df6b969d51cde9fd97fdf0d0b528fd1c48859e
CodeHunt7 <mishavasilenko1@yandex.ru>
2025-11-25 21:02:04 +0300

    Added cleanInput function and tests for it

---
+++ repl.go
@@ -0,0 +1,20 @@
+﻿package main
+
+import (
+	"strings"
+)
+
+func cleanInput(text string) []string {
+    // Fields сама разобьет по пробелам и уберет дублирующиеся пробелы
+    fields := strings.Fields(text)
+    
+    // Если слов много, лучше сразу выделить память (оптимизация)
+    output := make([]string, 0, len(fields))
+    
+    for _, word := range fields {
+        // Просто лоуеркейсим и добавляем
+        output = append(output, strings.ToLower(word))
+    }
+    
+    return output
+}
---
+++ repl_test.go
@@ -0,0 +1,39 @@
+﻿package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: "Mama give me PICKachoo",
+			expected: []string{"mama", "give", "me", "pickachoo"},
+		},
+	// add more cases here
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("cleanInput(%q) = %v; want %v", c.input, actual, c.expected)
+			t.Fail()
+		}
+		// Check the length of the actual slice against the expected slice
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("cleanInput(%q) = %v; want %v", c.input, actual, c.expected)
+				t.Fail()
+			}
+			// Check each word in the slice
+		}
+	}
+
+}
