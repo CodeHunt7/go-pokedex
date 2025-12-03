202e53da9e233ad97c3ed122c7cbade3eca7529d
CodeHunt7 <mishavasilenko1@yandex.ru>
2025-12-03 21:23:49 +0300

    Added Help and Exit commands to app

--- main.go
+++ main.go
@@ -6,7 +6,29 @@ import (
 	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
 func main() {
+	
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+	}
+	
 	scanner := bufio.NewScanner(os.Stdin)
 	
 	for {
@@ -15,7 +37,15 @@ func main() {
 		if scanner.Scan() {
 			userInput := scanner.Text()
 			firstWord := cleanInput(userInput)[0]
-			fmt.Printf("Your command was: %s\n", firstWord)
+			//fmt.Printf("Your command was: %s\n", firstWord)
+
+			if cmd, exists := commands[firstWord]; exists {
+				if err := cmd.callback(); err != nil {
+					fmt.Fprintf(os.Stderr, "Error executing command %q: %v\n", firstWord, err)
+				}
+			} else {
+				fmt.Println("Unknown command")
+			}
 		}
 
 		if err := scanner.Err(); err != nil {
--- repl.go
+++ repl.go
@@ -1,6 +1,8 @@
 ﻿package main
 
 import (
+	"fmt"
+	"os"
 	"strings"
 )
 
@@ -12,9 +14,25 @@ func cleanInput(text string) []string {
     output := make([]string, 0, len(fields))
     
     for _, word := range fields {
-        // Просто лоуеркейсим и добавляем
+        // Просто лоуеркейсим и добавляем в ответ
         output = append(output, strings.ToLower(word))
     }
     
     return output
+}
+
+func commandExit() error {
+    fmt.Println("Closing the Pokedex... Goodbye!")
+    os.Exit(0)
+    return nil
+}
+
+func commandHelp() error {
+    fmt.Println("Welcome to the Pokedex!")
+    fmt.Println("Usage:\n")
+    for _, cmd := range commands {
+        fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+    }
+    fmt.Println()
+    return nil
 }
